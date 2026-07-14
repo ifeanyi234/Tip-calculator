38da76c3b8dfb6ea1cf3995f6ff2fcbcde47a4dd
sabzyhacks <ei711283@gmail.com>
2026-07-14 22:59:11 +0100

    Added display result and reset function

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const tipInput = document.querySelector("#tip");
 const presetBtn = document.querySelectorAll("button[type='button']");
 const submit = document.querySelector("#submit");
 const error = document.querySelector(".error");
+const output = document.querySelector(".output");
 let tipPer = 0;
 
 // looping through preset button
@@ -21,21 +22,6 @@ presetBtn.forEach((btn) =>
   }),
 );
 
-// // calculate tip
-// const calcTip = function (bill, tipPercent) {
-//   return bill * (tipPercent / 100);
-// };
-
-// // calculate total
-// const calcTotal = function (bill, tipPercent) {
-//   return bill + calcTip(bill, tipPercent);
-// };
-
-// // Split that total by the number of people
-// const calcPerPerson = function (bill, tipPercent, personCount) {
-//   return calcTotal(bill, tipPercent) / personCount;
-// };
-
 // i was using pure functions for each calc (tip, total, perperson) but i felt like something was offf cause i was calling calctip in calctotal and calcTotal in calcperperson and if i had do call i need to pass parameters multiple times so gemini recommended i use object return method
 
 const calculateResult = function (bill, tipPercent, personCount) {
@@ -44,15 +30,23 @@ const calculateResult = function (bill, tipPercent, personCount) {
   const perPerson = total / personCount;
 
   return {
-    tip: tip,
-    total: total,
-    perPerson: perPerson,
+    tip: parseFloat(tip.toFixed(2)),
+    total: parseFloat(total.toFixed(2)),
+    perPerson: parseFloat(perPerson.toFixed(2)),
   };
+};
+
+const reset = function () {
+  billInput.value = "";
+  numPerson.value = "";
+  tipInput.value = "";
+  tipPer = 0;
+  presetBtn.forEach((b) => b.classList.remove("active"));
 };
 
 submit.addEventListener("click", function (e) {
   e.preventDefault();
-  const billValue = parseInt(billInput.value);
+  const billValue = parseFloat(billInput.value);
   const personValue = parseInt(numPerson.value);
 
   if (
@@ -69,8 +63,11 @@ submit.addEventListener("click", function (e) {
     error.classList.remove("active");
     const calcOutput = calculateResult(billValue, tipPer, personValue);
 
-    console.log("Tip:", calcOutput.tip);
-    console.log("Total:", calcOutput.total);
-    console.log("Per Person:", calcOutput.perPerson);
+    // Display
+    output.style.display = "flex";
+    output.innerHTML = `<div class="tip"><h2>Tip: ₦${calcOutput.tip}</h2><h2>Total: ₦${calcOutput.total}</h2><h2>Per Person: ₦${calcOutput.perPerson}</h2></div>`;
+
+    // Reset
+    reset();
   }
 });
